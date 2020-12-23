a7e94a68496fcb605d0a82d9adc0e0cf757a6f6b
吳俊緯 <wcw850710@gmail.com>
2020-12-23 09:34:00 +0800

    todo

--- src/@react-model/index.ts
+++ src/@react-model/index.ts
@@ -5,6 +5,7 @@ import { useReset } from './hook/useReset'
 import { publish } from './util/publish'
 import { memorize } from './util/memorize'
 
+// TODO 之後要必包傳方法出去，保證 rm_store 的封閉性及存在性
 export const $$rm_store: {
   subscribers: {
     [type: string]: Function[]
